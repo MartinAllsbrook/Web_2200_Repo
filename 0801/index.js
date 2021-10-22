389ccea0f67c91b2ffe240f6f9e8df2c349251b0
MartinAllsbrook <78395106+MartinAllsbrook@users.noreply.github.com>
2021-10-21 20:22:39 -0600

    stuff

--- 0801/index.js
+++ 0801/index.js
@@ -1,6 +1,10 @@
 const options = document.getElementsByClassName("type");
+const images = ["Didot.png", "AvantGarde.png", "Helvetica.jpg"];
+const titles = ["DIDOT", "AVANTGARDE", "HELVETICA"];
+
 
 for(i=0;i<options.length;i++){
-  console.log(i);
-  console.log(options[i].innerHTML);
+  // console.log(options[i].innerHTML);
+  document.getElementsByClassName('label')[i].innerHTML = titles[i];
+  options[i].innerHTML += '<img src="assets/'+images[i]+'">';
 }

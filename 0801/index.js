c14ef98918e6804d217dec824c4a8a707ae22f24
MartinAllsbrook <78395106+MartinAllsbrook@users.noreply.github.com>
2021-10-22 12:05:19 -0600

    asiytdy

--- 0801/index.js
+++ 0801/index.js
@@ -7,4 +7,10 @@ for(i=0;i<options.length;i++){
   // console.log(options[i].innerHTML);
   document.getElementsByClassName('label')[i].innerHTML = titles[i];
   options[i].innerHTML += '<img src="assets/'+images[i]+'">';
+  options[i].setAttribute("id", titles[i])
+  options[i].addEventListener("click", imageClicked);
+}
+
+function imageClicked() {
+  changeColor(this.id);
 }

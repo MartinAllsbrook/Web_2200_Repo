31eb733562634bafa7d41741dd35a2bc78e7f878
MartinAllsbrook <maal5844@colorado.edu>
2021-10-25 17:15:05 -0600

    Added comments and improved history
    History array not only contains the current positions of the snake. Also added alerts when the snake hits itself or the wall

--- 0902/index.js
+++ 0902/index.js
@@ -1,43 +1,55 @@
+// Check that the JS is working
 console.log("JS RUNNING");
 
 // Call grid generator
 genDivs(16);
 
-//Create snake
-
+// Create all variables for snake
+// Bools to track direction of snake
 let up = false;
 let right = false;
 let down = false;
 let left = false;
+// Strings to compare the position of the snakes head and tail to div ids
 let snakeHead = ["0 0"]
 let snakeTail = ["0 0"]
-let snakeLength = 3;
+// Int to tarack the snakes length
+let snakeLength = 8;
+// Ints to track the x & y positions of the snakes head and tail
 let xTail = 0;
 let yTail = 0;
 let xHead = 0;
 let yHead = 0;
+// Arays to track where the snake currently is
 let xHistory = [0];
 let yHistory = [0];
 
+
+// Event listener scanning for arrow key presses
 window.addEventListener('keydown', function(e) {
   // console.log('You pressed ' + e.key); // TESTING LINE
-  if(e.key == "ArrowUp"){
+  // If the ArrowUp key is pressed and the snake isnt going down, set the snakes current state to up
+  if(e.key == "ArrowUp" && !down){
     // console.log('Up Sucess'); // TESTING LINE
+    // Set up to true and all other directions to false
     up = true;
     right = false;
     down = false;
     left = false;
-  }else if(e.key == "ArrowRight"){
+  // Repeat with right/left
+  }else if(e.key == "ArrowRight" && !left){
     up = false;
     right = true;
     down = false;
     left = false;
-  }else if(e.key == "ArrowDown"){
+  // Repeat with down/up
+  }else if(e.key == "ArrowDown" && !up){
     up = false;
     right = false;
     down = true;
     left = false;
-  }else if(e.key == "ArrowLeft"){
+  // Repeat with left/right
+  }else if(e.key == "ArrowLeft" && !right){
     up = false;
     right = false;
     down = false;
@@ -52,67 +64,97 @@ window.addEventListener('keydown', function(e) {
 // console.log(document.getElementById(snakeHead).className);
 // console.log(document.getElementById(snakeHead).className == "cell cellOn");
 
+// Turn on the top left cell to indicate starting position
 document.getElementById(snakeHead).classList.toggle('cellOn')
+
+// Call main(), everything below this is functions
 main()
 
+//--------------------------------------------------------------------------------------------------------------//
+
+// main() waits for the player to press a key, and then starts the game
 function main() {
+  // Run this function repeatedly every 100milisec
   setTimeout(function onTick() {
+    // Only accepts a right or down keypress because left and up would immeadiately end the game
+    // If one of these keys is pressed start the game with gameOn()
     if(right||down){
       gameOn();
+    // Else call main again till a key is pressed
     }else{
-      // Call main again till game starts
       main();
     }
   }, 100)
 }
 
-function waitToStart(){
-
-}
-
 function gameOn() {
+  // Run this function repeatedly every 100milisec
   setTimeout(function onTick() {
     // console.log("up: "+ up); // TESTING LINE
     // console.log("right: "+ right); // TESTING LINE
     // console.log("down: "+ down); // TESTING LINE
     // console.log("left: "+ left); // TESTING LINE
 
-    // move xHead up if up arrow hit
+    // Change xHead & yHead based on the snakes current movement state / the player's last button press
     if(up){
+      // Must ask if yHead > 0 to keep the snake within the top bound
       if(yHead > 0){
+        // Decrease yHead by one to move snake
         yHead--;
+      }else{
+        alert("Snake hit wall, Game Over :(")
       }
     }else if(down){
+      // Must ask if yHead < 15 to keep the snake within the bottom bound
       if(yHead < 15){
         yHead++;
+      }else{
+        alert("Snake hit wall, Game Over :(")
       }
     }else if(left){
+      // Must ask if xHead > 0 to keep the snake within the left bound
       if(xHead > 0){
         xHead--;
+      }else{
+        alert("Snake hit wall, Game Over :(")
       }
     }else if(right){
+      // Must ask if xHead < 15 to keep the snake within the right bound
       if(xHead < 15){
         xHead++;
+      }else{
+        alert("Snake hit wall, Game Over :(")
       }
     }
 
+    // Put the snake's head location into an array of where the snake has been
     xHistory.push(xHead);
     yHistory.push(yHead);
+
+    // Set snakeHead string to hold the snakeHead's current location
     snakeHead = [xHead + " " + yHead];
-    console.log(snakeHead);
+    // console.log(snakeHead); // TESTING LINE;
     if(document.getElementById(snakeHead).className == "cell") {
       document.getElementById(snakeHead).classList.toggle('cellOn');
+    }else{
+      alert("Snake hit it's self, Game Over :(")
     }
+
+    // Once the snake has moved enough start removing it's tail
     if(xHistory.length > snakeLength){
-      xTail = [xHistory[xHistory.length - 1 - snakeLength]]
-      yTail = [yHistory[yHistory.length - 1 - snakeLength]]
-      //console.log(xHistory + " , " + yHistory) // TESTING LINE
+      // Set the tail location to be snakeLength units into the snakes history
+      xTail = [xHistory[xHistory.length - 1 - snakeLength]];
+      yTail = [yHistory[yHistory.length - 1 - snakeLength]];
       snakeTail = [xTail + " " + yTail];
-      //xHistory.splice(xHistory.length - 1 - snakeLength)
-      //yHistory.splice(yHistory.length - 1 - snakeLength)
-      console.log(snakeTail);
+      // Remove the items in snake history that are no longer part of the snake
+      xHistory.splice(xHistory.length - 1 - snakeLength, 1);
+      yHistory.splice(yHistory.length - 1 - snakeLength, 1);
+
+      // console.log(xHistory); // TESTING LINE
+      // console.log(yHistory); // TESTING LINE
+      // console.log(snakeTail); // TESTING LINE
       if(document.getElementById(snakeTail).className == "cell cellOn") {
-        console.log("yes");
+        // console.log("yes"); // TESTING LINE
         document.getElementById(snakeTail).classList.toggle('cellOn');
       }
     }
@@ -136,7 +178,7 @@ function genDivs(v){
             // Generate a div with class name "cell"
             var cell = document.createElement("div");
             cell.className = "cell";
-            // Set the id of each cell to be its prosition in the grid left-->right, top-->bottom
+            // Set the id of each cell to be its coordinates in the grid x = left-->right, y = top-->bottom
             cell.setAttribute("id", x + " " + i);
             // Add the new div to the current row
             row.appendChild(cell);

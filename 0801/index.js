2aee178f98478b1c2863784715edfba00f061e06
MartinAllsbrook <78395106+MartinAllsbrook@users.noreply.github.com>
2021-11-12 20:17:55 -0700

    puhs

--- 0801/index.js
+++ 0801/index.js
@@ -2,22 +2,28 @@ const options = document.getElementsByClassName("type");
 const images = ["Didot.png", "AvantGarde.png", "Helvetica.jpg"];
 const titles = ["DIDOT", "AVANTGARDE", "HELVETICA"];
 let prevChoice;
+let meal;
 
 
 for(i=0;i<options.length;i++){
+  options[i].id = titles[i];
   options[i].innerHTML = "<h2>" + titles[i] + "</h2>";
   options[i].addEventListener("click", setActive, false);
-  options[i].id = titles[i];
 }
+
+document.getElementById('DIDOT').classList.toggle('active');
+document.getElementById('image').innerHTML = "<img src= 'assets/" + images[0] + "'>";
 
 // e = event
 function setActive(e){
-
-  prevChoice = document.getElementsByClassName()
+  if(document.getElementsByClassName("active")[0] != undefined){
+    prevChoice = document.getElementsByClassName("active")[0].id;
+    // console.log(document.getElementsByClassName("active")[0].id);
+  }
   if(prevChoice != undefined){
     document.getElementById(prevChoice).classList.toggle("active");
   }
-  console.log("prevChoice " + prevChoice);
+  // console.log("prevChoice " + prevChoice);
 
   // make sure we dont just target inner html
   if(e.target.tagName == "H2"){
@@ -27,4 +33,8 @@ function setActive(e){
     console.log("div clicked");
     e.target.classList.toggle("active");
   }
+
+  meal = document.getElementsByClassName("active")[0].id;
+  console.log(meal);
+  // document.getElementById('image').innerHTML = "<img src= 'assets/" + images[0] + "'>"
 }

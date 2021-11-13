dfdfeaed075216a81e3bfda8ce4dabcc05637c99
MartinAllsbrook <maal5844@colorado.edu>
2021-11-13 00:00:23 -0700

    push

--- 0801/index.js
+++ 0801/index.js
@@ -1,19 +1,20 @@
 const options = document.getElementsByClassName("type");
-const images = ["Didot.png", "AvantGarde.png", "Helvetica.jpg"];
-const titles = ["DIDOT", "AVANTGARDE", "HELVETICA"];
+const images = ["Didot.png", "AvantGarde.png", "Helvetica.png", "Garamond.png", "Futura.png"];
+const titles = ["Didot", "AvantGarde", "Helvetica", "Garamond", "Futura"];
 let prevChoice;
 let meal;
 
 
 for(i=0;i<options.length;i++){
   options[i].id = titles[i];
-  options[i].innerHTML = "<h2>" + titles[i] + "</h2>";
+  // options[i].innerHTML = "<h2>" + titles[i] + "</h2>";
   options[i].addEventListener("click", setActive, false);
 }
 
-document.getElementById('DIDOT').classList.toggle('active');
+document.getElementById('Didot').classList.toggle('active');
 document.getElementById('image').innerHTML = "<img src= 'assets/" + images[0] + "'>";
-
+document.getElementById('back').addEventListener('click', function(){
+  location.href='../index.html'});
 // e = event
 function setActive(e){
   if(document.getElementsByClassName("active")[0] != undefined){
@@ -27,14 +28,14 @@ function setActive(e){
 
   // make sure we dont just target inner html
   if(e.target.tagName == "H2"){
-    console.log("h2 clicked");
+    // console.log("h2 clicked");
     e.target.parentNode.classList.toggle("active");
   }else{
-    console.log("div clicked");
+    // console.log("div clicked");
     e.target.classList.toggle("active");
   }
 
-  meal = document.getElementsByClassName("active")[0].id;
-  console.log(meal);
-  // document.getElementById('image').innerHTML = "<img src= 'assets/" + images[0] + "'>"
+  current = document.getElementsByClassName("active")[0].id;
+  // console.log(current);
+  document.getElementById('image').innerHTML = "<img src= 'assets/" + current + ".png'>"
 }
